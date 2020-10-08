b4424cc7a24251eddb8f0ac41048a3f047b8ca74
rene-INTech <remi.lamiaux@telecom-sudparis.eu>
2020-10-08 13:18:30 +0200

    Ajout mesure pression

--- testNucleo/Core/Src/main.c
+++ testNucleo/Core/Src/main.c
@@ -64,15 +64,28 @@ static void MX_I2C1_Init(void);
 static void MX_RF_Init(void);
 static void MX_RTC_Init(void);
 /* USER CODE BEGIN PFP */
-int32_t BME280_compensate_T(int32_t adc_T);
-void HAL_Delay(uint32_t Delay);
+int32_t BME280_compensate_T(int32_t);
+uint32_t BME280_compensate_P_int64(int32_t);
+void HAL_Delay(uint32_t);
 /* USER CODE END PFP */
 
 /* Private user code ---------------------------------------------------------*/
 /* USER CODE BEGIN 0 */
+static int32_t t_fine;
+
 static uint16_t dig_T1;
 static int16_t dig_T2;
 static int16_t dig_T3;
+
+static uint16_t dig_P1;
+static int16_t dig_P2;
+static int16_t dig_P3;
+static int16_t dig_P4;
+static int16_t dig_P5;
+static int16_t dig_P6;
+static int16_t dig_P7;
+static int16_t dig_P8;
+static int16_t dig_P9;
 /* USER CODE END 0 */
 
 /**
@@ -83,9 +96,11 @@ int main(void)
 {
   /* USER CODE BEGIN 1 */
 	uint8_t bufUart[20];
-	uint8_t bufI2C[12];
+	uint8_t bufI2C[20];
 	int32_t adc_T;
+	int32_t adc_P;
 	int32_t temperature;
+	uint32_t pressure;
   /* USER CODE END 1 */
 
   /* MCU Configuration--------------------------------------------------------*/
@@ -123,11 +138,8 @@ int main(void)
 
   bufI2C[0] = 0xF4; // "ctrl_meas" register
   bufI2C[1] = 0b001 << 5 | 0b11; // oversampling = 1 for temperature, normal mode
-  HAL_I2C_Master_Transmit(&hi2c1, BME_ADDR, bufI2C, 2, HAL_MAX_DELAY); //send 2 bytes
-
-  bufI2C[0] = 0xF5; // "config" register
-  bufI2C[1] = 0b100 << 5; // 1 measure every 500ms
-  HAL_I2C_Master_Transmit(&hi2c1, BME_ADDR, bufI2C, 2, HAL_MAX_DELAY); //send 2 bytes
+  bufI2C[2] = 0b100 << 5; // 1 measure every 500ms
+  HAL_I2C_Master_Transmit(&hi2c1, BME_ADDR, bufI2C, 3, HAL_MAX_DELAY); //send 3 bytes
 
   bufI2C[0] = 0x88; // "dig_Tx" registers
   HAL_I2C_Master_Transmit(&hi2c1, BME_ADDR, bufI2C, 1, HAL_MAX_DELAY); //send 1 bytes
@@ -136,6 +148,16 @@ int main(void)
   dig_T1 = bufI2C[0] | (uint16_t)bufI2C[1] << 8;
   dig_T2 = bufI2C[2] | (uint16_t)bufI2C[3] << 8;
   dig_T3 = bufI2C[4] | (uint16_t)bufI2C[5] << 8;
+  HAL_I2C_Master_Receive(&hi2c1, BME_ADDR, bufI2C, 18, HAL_MAX_DELAY); //receive 18 bytes
+  dig_P1 = bufI2C[0] | (uint16_t)bufI2C[1] << 8;
+  dig_P2 = bufI2C[2] | (uint16_t)bufI2C[3] << 8;
+  dig_P3 = bufI2C[4] | (uint16_t)bufI2C[5] << 8;
+  dig_P4 = bufI2C[6] | (uint16_t)bufI2C[7] << 8;
+  dig_P5 = bufI2C[8] | (uint16_t)bufI2C[9] << 8;
+  dig_P6 = bufI2C[10] | (uint16_t)bufI2C[11] << 8;
+  dig_P7 = bufI2C[12] | (uint16_t)bufI2C[13] << 8;
+  dig_P8 = bufI2C[14] | (uint16_t)bufI2C[15] << 8;
+  dig_P9 = bufI2C[16] | (uint16_t)bufI2C[17] << 8;
 
   strcpy((char*)bufUart, "INIT COMPLETED\r\n");
   HAL_UART_Transmit(&huart1, bufUart, strlen((char*)bufUart), HAL_MAX_DELAY);
@@ -152,15 +174,22 @@ int main(void)
 	  //HAL_UART_Transmit(&huart1, buf, strlen((char*)buf), HAL_MAX_DELAY);
 	  HAL_Delay(500);
 
-	  bufI2C[0] = 0xFA; // temperature MSB register
+	  bufI2C[0] = 0xF7; // temperature MSB register
 	  HAL_I2C_Master_Transmit(&hi2c1, BME_ADDR, bufI2C, 1, HAL_MAX_DELAY); // send 1 byte
-	  HAL_I2C_Master_Receive(&hi2c1, BME_ADDR, bufI2C, 3, HAL_MAX_DELAY); // receive 3 bytes
 
+	  HAL_I2C_Master_Receive(&hi2c1, BME_ADDR, bufI2C, 3, HAL_MAX_DELAY); // receive 3 bytes
+	  adc_P = (int32_t)bufI2C[0] << 12 | (int32_t)bufI2C[1] << 4 | bufI2C[2] >> 4;
+
+	  HAL_I2C_Master_Receive(&hi2c1, BME_ADDR, bufI2C, 3, HAL_MAX_DELAY); // receive 3 bytes
 	  adc_T = (int32_t)bufI2C[0] << 12 | (int32_t)bufI2C[1] << 4 | bufI2C[2] >> 4;
+
 	  temperature = BME280_compensate_T(adc_T);
-	  sprintf(bufUart, "%.2f\r\n", temperature/100.0);
+	  sprintf(bufUart, "T=%.2f °C\r\n", temperature/100.0);
 	  HAL_UART_Transmit(&huart1, bufUart, strlen((char*)bufUart), HAL_MAX_DELAY);
 
+	  pressure = BME280_compensate_P_int64(adc_P);
+	  sprintf(bufUart, "P=%.3f hPa\r\n", pressure/256.0/100.0);
+	  HAL_UART_Transmit(&huart1, bufUart, strlen((char*)bufUart), HAL_MAX_DELAY);
 
 	  if(temperature < 2500) { // if T < 25°C
 		  HAL_GPIO_WritePin(GPIOB, GPIO_PIN_5, GPIO_PIN_SET);
@@ -487,12 +516,37 @@ static void MX_GPIO_Init(void)
 /* USER CODE BEGIN 4 */
 // From BME280 datasheet
 int32_t BME280_compensate_T(int32_t adc_T){
-	int32_t var1, var2, T, t_fine;
+	int32_t var1, var2, T;
 	var1 = ((((adc_T>>3) - ((int32_t)dig_T1<<1))) * ((int32_t)dig_T2))>>11;
 	var2 = (((((adc_T>>4) - ((int32_t)dig_T1)) * ((adc_T>>4) -  ((int32_t)dig_T1))) >> 12) * ((int32_t)dig_T3)) >> 14;
 	t_fine = var1+var2;
 	T = (t_fine *5 + 128) >> 8;
 	return T;
+}
+
+/**
+ * Returns pressure in Pa as unsigned 32 bit integer in Q24.8 format
+ * (24 integer bits and 8 fractional bits)
+ * Output value of "24674867" represents 24674867/256 = 96386.2 Pa = 963.862 hPa
+ * Warning: the BME280_compensate_T function must have been called before calling this function
+ */
+uint32_t BME280_compensate_P_int64(int32_t adc_P) {
+	int64_t var1, var2, p;
+	var1=((int64_t)t_fine)-128000;
+	var2 = var1 * var1 * (int64_t)dig_P6;
+	var2 = var2 + ((var1*(int64_t)dig_P5)<<17);
+	var2 = var2 + (((int64_t)dig_P4)<<35);
+	var1 = ((var1 * var1 * (int64_t)dig_P3)>>8) + ((var1 * (int64_t)dig_P2)<<12);
+	var1 = (((((int64_t)1)<<47)+var1))*((int64_t)dig_P1)>>33;
+	if(var1 == 0) {
+		return 0; // avoid exception caused by division by 0
+	}
+	p = 1048576-adc_P;
+	p = (((p<<31)-var2)*3125)/var1;
+	var1 = (((int64_t)dig_P9) * (p>>13) * (p>>13)) >> 25;
+	var2 = (((int64_t)dig_P8) * p) >> 19;
+	p = ((p + var1 + var2) >> 8) + (((int64_t)dig_P7)<<4);
+	return (uint32_t)p;
 }
 
 /*************************************************************
